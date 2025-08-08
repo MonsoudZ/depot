4a8f3d850069782a3fc65d356eab127edcab6b8f
Monsoud Zanaty <monsoud.zanaty@gmail.com>
2025-08-07 23:12:23 -0400

    Added new view files, and controllers

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,20 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   has_one_attached :image
   after_commit -> { broadcast_refresh_later_to "products"}
+
+  validates :title, :decription, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items_empty?
+      errors.add(:base, "Line Items Present")
+      throw :abort
+    end
+  end
 end
